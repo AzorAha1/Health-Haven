c39daf702667da2f0903b07d3b1a85315359df5c
NeneNafe <nenechiwaya@gmail.com>
2024-02-29 16:06:43 +0200

    App file to run all the routes

--- backend_db/app.py
+++ backend_db/app.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     """This brings a user to the home page of the app"""
-    return 'The main page'
+    return "The main page"
+
+
+if __name__ == "__main__":
+    app.run()
