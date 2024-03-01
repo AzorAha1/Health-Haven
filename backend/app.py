d6aac5210d0df9675a7465d3d6f10f0990abc41e
AzorAha1 <wizfaiz@icloud.com>
2024-03-01 10:46:46 +0100

    templates set

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../frontend/templates')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     """This brings a user to the home page of the app"""
-    return "The main page"
+    return render_template('home.html', title='Home')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5030, debug=True)
