6d6afd92130fe969ea5bbb90b7a4fdfb2a09f451
AzorAha1 <wizfaiz@icloud.com>
2024-03-04 12:16:21 +0100

    design for authentication done

--- backend/auth.py
+++ backend/auth.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField, SubmitField
+from wtforms import StringField, PasswordField, EmailField, SubmitField, DateField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 class RegisterPatient(FlaskForm):
-    username = StringField(label='username', validators=[DataRequired()])
-    email = EmailField(label='Email', validators=[DataRequired(), Email()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
-    confirm_password = PasswordField(label='Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField(label='Submit')
+    username = StringField(label='Username', validators=[DataRequired()], render_kw={'class': 'design-field'})
+    email = EmailField(label='Email', validators=[DataRequired(), Email()], render_kw={'class': 'design-field'})
+    password = PasswordField(label='Password', validators=[DataRequired()],render_kw={'class': 'design-field'})
+    dob = DateField(validators=[DataRequired()], label='Date of birth')
+    confirm_password = PasswordField(label='Confirm Password', validators=[DataRequired(), EqualTo('password')],render_kw={'class': 'design-field'})
+    submit = SubmitField(label='Submit',render_kw={'class': 'submit-design'})
 
 class LoginPatient(FlaskForm):
     email = EmailField(label='Email', validators=[DataRequired(), Email()], render_kw={'class': 'design-field'})
